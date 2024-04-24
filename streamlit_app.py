4059f3555e715fa63b444fb5f8173cb7ded196f7
Klaus Wiegand <kwiegand@stoneridgetechnology.com>
2024-04-23 21:39:17 -0500

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   avatar.png
    	new file:   docs/10. Nonarthritic Hip Joint Pain - June 2014.pdf
    	new file:   docs/14. Ankle Stability & Movement Coordination Impairments-Ankle Sprain - September 2013.pdf
    	new file:   docs/3. Shoulder Pain and Mobility Deficits-Adhesive Capsulitis - May 2013.pdf
    	new file:   docs/5th-metatarsal-repair.pdf
    	new file:   docs/AMCA-PTTC-Study-Guide.pdf
    	new file:   docs/Knee Ligament Sprain CPG - 2017.pdf
    	new file:   docs/Knee pain and mobility impairments 2018.pdf
    	new file:   docs/PHYSICAL-THERAPY.pdf
    	new file:   docs/Physical Therapy 101 |Everything You Need to Know as a Patient.pdf
    	new file:   docs/ac-joint-reconstruction.pdf
    	new file:   docs/achilles-repair.pdf
    	new file:   docs/achilles-tear-non-op-rehab-protocol.pdf
    	new file:   docs/acl-r-guidelines-final-22.pdf
    	new file:   docs/ankle-arthroscopy-w-or-w-o-talar-ocd-microfx.pdf
    	new file:   docs/anterior-instability-repair.pdf
    	new file:   docs/arthroscopic-and-open-capsular-shift.pdf
    	new file:   docs/biceps-tenotomy-and-tenodesis.pdf
    	new file:   docs/coracoid-transfer-and-glenoid-bone-grafting-for-anterior-instability-with-bone-loss.pdf
    	new file:   docs/cpt7_study_guide.pdf
    	new file:   docs/distal-biceps-repair.pdf
    	new file:   docs/elbow-arthroscopy-and-capsular-release.pdf
    	new file:   docs/elbow-arthroscopy-and-debridement.pdf
    	new file:   docs/elbow-arthroscopy-and-lateral-epicondyle-debridement.pdf
    	new file:   docs/guide_educator_module.pdf
    	new file:   docs/hemiarthroplasty-for-proximal-humerus-fracture.pdf
    	new file:   docs/high-tibial-osteotomy-or-distal-femoral-osteotomy.pdf
    	new file:   docs/knee-acl-pcl-lcl-reconstruction.pdf
    	new file:   docs/knee-acl-pcl-mcl-lcl-reconstruction.pdf
    	new file:   docs/knee-acl-pcl-mcl-reconstruction.pdf
    	new file:   docs/knee-acl-pcl-reconstruction.pdf
    	new file:   docs/knee-arthroscopy-meniscectomy-chondroplasty-loose-body-removal.pdf
    	new file:   docs/knee-lcl-and-posterolateral-corner-repair-and-reconstruction.pdf
    	new file:   docs/knee-mcl-reconstruction.pdf
    	new file:   docs/knee-ocd-repair-and-oats.pdf
    	new file:   docs/knee-patellofemoral-microfracture.pdf
    	new file:   docs/knee-tibiofemoral-microfracture.pdf
    	new file:   docs/martin-et-al-2018-achilles-pain-stiffness-and-muscle-power-deficits-midportion-achilles-tendinopathy-revision-2018.pdf
    	new file:   docs/meniscal-allograft-transplantation.pdf
    	new file:   docs/meniscus-repair-guidelines-final-22.pdf
    	new file:   docs/mpfl.pdf
    	new file:   docs/mtss-guidelines.pdf
    	new file:   docs/posterior-instability-repair.pdf
    	new file:   docs/proximal-humerus-and-greater-tuberosity-orif.pdf
    	new file:   docs/quadriceps-or-patellar-tendon-repair.pdf
    	new file:   docs/rehab-patellar-tendon-debridement.pdf
    	new file:   docs/rotator-cuff-repair-post-op.pdf
    	new file:   docs/shoulder-general-post-operative-instructions-slu.pdf
    	new file:   docs/shoulder-manipulation-and-shoulder-arthroscopic-capsular-release.pdf
    	new file:   docs/shoulder-subacromial-decompression-and-distal-clavicle-excision.pdf
    	new file:   docs/slap-repair.pdf
    	new file:   docs/slu-anatomic-tsa-final.pdf
    	new file:   docs/subscapularis-repair.pdf
    	new file:   docs/tibial-spine-repair.pdf
    	new file:   docs/ucl-ligament-reconstruction-22.pdf
    	new file:   docs/ucl-repair-guidelines-final.pdf
    	new file:   docs/willy-et-al-2019-patellofemoral-pain.pdf
    	new file:   open_ai_data/embeddings_model_id
    	new file:   open_ai_data/index.faiss
    	new file:   open_ai_data/index.pkl
    	new file:   requirements.txt
    	new file:   streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,148 @@
+import streamlit as st
+
+# Initialize session state to store conversation history
+if 'history' not in st.session_state:
+    st.session_state.history = []
+    st.session_state.input_key = 0
+
+    # We also need to build the variables needed for the chat and load the vector store
+    # Import needed packages
+    #
+    # Standard Stuff
+    #
+    import os
+    import sys
+
+    # We use itemgetter in our chains
+    #
+    from   operator  import itemgetter
+
+    # A lot of stuff from langchain 
+    #
+    from   langchain.prompts import PromptTemplate,MessagesPlaceholder
+    from   langchain.prompts.chat import ChatPromptTemplate
+    from   langchain.chains import LLMChain
+    from   langchain_community.vectorstores import FAISS
+    from   langchain.chains import RetrievalQA
+    from   langchain_community.docstore.document import Document
+    from   langchain.chains import ConversationalRetrievalChain
+    from   langchain.memory import ConversationBufferMemory
+    from   langchain.schema import format_document
+    from   langchain.schema.messages import AIMessage, HumanMessage, get_buffer_string
+    from   langchain_core.runnables import RunnableParallel
+    from   langchain_core.runnables import RunnableLambda, RunnablePassthrough
+    from   langchain_openai import ChatOpenAI
+    from   openai import OpenAI
+    from   langchain_openai import OpenAIEmbeddings
+
+    # NumPy
+    #
+    import numpy as np
+
+    os.environ['OPENAI_API_KEY'] = st.secrets.ai_credentials.openai_api_key
+    st.session_state.model_kwargs = { 'top_p' : 0.1 }
+    st.session_state.llm = ChatOpenAI(temperature=0.1, model_name="gpt-4", model_kwargs = st.session_state.model_kwargs)
+ #  st.session_state.llm = ChatOpenAI(temperature=0.1, model_name="gpt-3.5-turbo", model_kwargs = st.session_state.model_kwargs)
+    EMBEDDING_MODEL_ID = 'text-embedding-3-small'
+    st.session_state.embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL_ID)
+
+    # Function to load vector store
+    def load_local_vector_store(vector_store_path):
+        with st.spinner(text="Initializing"):
+            try:
+                with open(f"{vector_store_path}/embeddings_model_id", 'r') as f:
+                    embeddings_model_id = f.read()
+                vector_store = FAISS.load_local(vector_store_path, st.session_state.embeddings)
+                return vector_store
+            except Exception:
+                st.stop()
+
+    # Load vector storage from disk
+    #
+    st.session_state.vector_store_path='./open_ai_data'
+    st.session_state.db = load_local_vector_store(st.session_state.vector_store_path)
+    st.session_state.retriever = st.session_state.db.as_retriever(search_type="similarity", search_kwargs={"k": 8})
+
+    st.session_state.qa_template = """
+    You are a doctor of physical therapy, treating patients and educating students at a university
+    clinic. You have excellent knowledge about treatment protocols and general medicine and are 
+    assisting young PT students in their examp preparition. Before you answer a question you always
+    take a step back and think about the meaning and intend of the question and then answer
+    the question to the besto of your knowledge, always keeping you educational role in mind.
+    Given the following [Chat History] and provided [Context], please respond to the student's [Question]. Don't preceede
+    your answer with "[Answer] :" or similar prefixes. Also, don't be too general when answering 
+    a question. If the student asks for an example then give a concrete example and the if the example
+    involves a concrete question then give a concrete answer.
+
+    [Chat History]: {chat_history}
+
+    [Context]: {context}
+
+    [Question]: {question}
+    """
+
+    st.session_state.QA_PROMPT       = ChatPromptTemplate.from_template(st.session_state.qa_template)
+    st.session_state.DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
+
+    def _combine_documents(docs, document_prompt=st.session_state.DOCUMENT_PROMPT, document_separator="\n\n"):
+        doc_strings = [format_document(doc, document_prompt) for doc in docs]
+        return document_separator.join(doc_strings)
+
+    st.session_state.memory = ConversationBufferMemory(llm=st.session_state.llm, return_messages=True, output_key="answer", input_key="question")
+
+    st.session_state.loaded_memory = RunnablePassthrough.assign(
+        chat_history=RunnableLambda(st.session_state.memory.load_memory_variables) | itemgetter("history")
+    )
+
+    st.session_state.retrieved_documents = {
+        "docs"        : itemgetter("question") | st.session_state.retriever,   # Please note that all second entries need to be callables(!)
+        "question"    : lambda x: x["question"],
+        "chat_history": lambda x: x["chat_history"]
+    }
+
+    st.session_state.final_inputs = {
+        "context"     : lambda x: _combine_documents(x["docs"]),
+        "question"    : itemgetter("question"),
+        "chat_history": itemgetter("chat_history")
+    }
+
+    st.session_state.answer = {
+        "answer"  : st.session_state.final_inputs | st.session_state.QA_PROMPT | st.session_state.llm,
+        "question": itemgetter("question"),
+    }
+
+    st.session_state.final_chain = st.session_state.loaded_memory | st.session_state.retrieved_documents | st.session_state.answer
+
+
+st.session_state.col1 = st.columns(1)
+
+# Main display area for text
+st.text_area("Conversation - please scroll to see latest answer", value="\n".join(st.session_state.history), height=550, key='conversation_{st.session_state.input_key}', disabled=False)
+
+# Function to update conversation history
+def update_history():
+    #
+    # Prepare the input for the RAG model
+    user_input  = st.session_state[f'input_{st.session_state.input_key}']
+    inputs = {"question": user_input}
+
+    # Invoke the RAG model to get an answer
+    result = st.session_state.final_chain.invoke(inputs)
+    
+    # Save the current question and its answer to memory for future context
+    #
+    st.session_state.memory.save_context(inputs, {"answer": result["answer"].content})
+
+    # Update conversation history
+    st.session_state.history.extend([f"You: {user_input}", " ", result["answer"].content, " "])
+    st.session_state.input_key += 1
+
+# User input
+st.text_input("Please state your question here - Press ENTER when you are finished", key=f'input_{st.session_state.input_key}', on_change=update_history, max_chars=200)  # Adjust max_chars as needed
+
+st.write("")
+st.write("")
+st.image('avatar.png', caption='Physical Therapist')
+st.write("For educational purposes only!")
+st.write("Chatbot may give wrong answers!")
+
